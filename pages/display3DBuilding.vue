00f3b8e0446e31d0dc08f7487d2e27f03d4c4208
Sonali-Mali93 <sonalimali999@gmail.com>
2022-09-27 15:53:02 +0530

    feat:  displays building heights in 3D

---
+++ pages/display3DBuilding.vue
@@ -0,0 +1,151 @@
+<template>
+    <div>
+        <main class="w-screen h-screen">
+            <v-map class="w-full h-full" :options="state.map" @loaded="onMapLoaded" />
+            <div id="menu">
+                <input id="satellite-v9" type="radio" name="rtoggle" value="satellite" checked="checked" />
+                <!-- See a list of Mapbox-hosted public styles at -->
+                <!-- https://docs.mapbox.com/api/maps/styles/#mapbox-styles -->
+                <label for="satellite-v9">satellite</label>
+                <input id="light-v10" type="radio" name="rtoggle" value="light" />
+                <label for="light-v10">light</label>
+                <input id="dark-v10" type="radio" name="rtoggle" value="dark" />
+                <label for="dark-v10">dark</label>
+                <input id="streets-v11" type="radio" name="rtoggle" value="streets" />
+                <label for="streets-v11">streets</label>
+                <input id="outdoors-v11" type="radio" name="rtoggle" value="outdoors" />
+                <label for="outdoors-v11">outdoors</label>
+            </div>
+        </main>
+        <p>{{state.data}}</p>
+    </div>
+</template>
+<script setup lang="ts">
+// import "mapbox-gl/dist/mapbox-gl.css";
+// import "v-mapbox/dist/v-mapbox.css";
+///
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import VMap from "v-mapbox";
+import mapboxgl from "mapbox-gl";
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+mapboxgl.accessToken =
+    "pk.eyJ1Ijoic29jaWFsZXhwbG9yZXIiLCJhIjoiREFQbXBISSJ9.dwFTwfSaWsHvktHrRtpydQ";
+const state = reactive({
+    map: {
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: [-74.0066, 40.7135],
+        zoom: 15.5,
+        pitch: 45,
+        bearing: -17.6,
+        container: 'map',
+        antialias: true
+    },
+    data: []
+});
+
+function onMapLoaded(map: mapboxgl.Map) {
+
+    // Insert the layer beneath any symbol layer.
+    const layers = map.getStyle().layers;
+    const labelLayerId = layers.find(
+        (layer) => layer.type === 'symbol' && layer.layout['text-field']
+    ).id;
+
+    // The 'building' layer in the Mapbox Streets
+    // vector tileset contains building height data
+    // from OpenStreetMap.
+    map.addLayer(
+        {
+            'id': 'add-3d-buildings',
+            'source': 'composite',
+            'source-layer': 'building',
+            'filter': ['==', 'extrude', 'true'],
+            'type': 'fill-extrusion',
+            'minzoom': 15,
+            'paint': {
+                'fill-extrusion-color': '#aaa',
+
+                // Use an 'interpolate' expression to
+                // add a smooth transition effect to
+                // the buildings as the user zooms in.
+                'fill-extrusion-height': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'height']
+                ],
+                'fill-extrusion-base': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'min_height']
+                ],
+                'fill-extrusion-opacity': 0.6
+            }
+        },
+        labelLayerId
+    );
+
+
+    //Add a vector tile source
+    map.addControl(
+        new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            mapboxgl: mapboxgl,
+        })
+    );
+    //Map Navigation Control
+    map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+
+    //Display globe on webpage
+    map.setFog({});
+
+}
+  //}
+</script>
+<style>
+body {
+    margin: 0;
+    padding: 0;
+}
+
+#map {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+}
+
+.marker {
+    background-image: url("http://localhost:3000/assets/images/download.png");
+    background-size: cover;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    cursor: pointer;
+}
+
+.mapboxgl-popup {
+    max-width: 200px;
+}
+
+.mapboxgl-popup-content {
+    text-align: center;
+    font-family: "Open Sans", sans-serif;
+}
+
+#menu {
+    position: absolute;
+    background: #EFEFEF;
+    padding: 10px;
+    font-family: "Open Sans", sans-serif;
+    margin-top: 600px;
+}
+</style>
