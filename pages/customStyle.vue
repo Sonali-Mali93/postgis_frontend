a9ab9fc50e99c198c856c044681b8a1533f30c13
Sonali-Mali93 <sonalimali999@gmail.com>
2022-09-27 15:14:57 +0530

    feat: display the map with custom style

---
+++ pages/customStyle.vue
@@ -0,0 +1,98 @@
+<template>
+    <div>
+        <main class="w-screen h-screen">
+            <v-map class="w-full h-full" :options="state.map" @loaded="onMapLoaded" />
+            <div id="menu">
+                <input id="satellite-v9" type="radio" name="rtoggle" value="satellite" checked="checked" />
+                <!-- See a list of Mapbox-hosted public styles at -->
+                <!-- https://docs.mapbox.com/api/maps/styles/#mapbox-styles -->
+                <label for="satellite-v9">satellite</label>
+                <input id="light-v10" type="radio" name="rtoggle" value="light" />
+                <label for="light-v10">light</label>
+                <input id="dark-v10" type="radio" name="rtoggle" value="dark" />
+                <label for="dark-v10">dark</label>
+                <input id="streets-v11" type="radio" name="rtoggle" value="streets" />
+                <label for="streets-v11">streets</label>
+                <input id="outdoors-v11" type="radio" name="rtoggle" value="outdoors" />
+                <label for="outdoors-v11">outdoors</label>
+            </div>
+        </main>
+        <p>{{state.data}}</p>
+    </div>
+</template>
+<script setup lang="ts">
+// import "mapbox-gl/dist/mapbox-gl.css";
+// import "v-mapbox/dist/v-mapbox.css";
+///
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import VMap from "v-mapbox";
+import mapboxgl from "mapbox-gl";
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+mapboxgl.accessToken =
+    "pk.eyJ1Ijoic29jaWFsZXhwbG9yZXIiLCJhIjoiREFQbXBISSJ9.dwFTwfSaWsHvktHrRtpydQ";
+const state = reactive({
+    map: {
+        container: "map",
+        style: 'mapbox://styles/examples/cke97f49z5rlg19l310b7uu7j',
+        zoom: 2,
+        center: [30, 50],
+    },
+    data: []
+});
+</script>
+<style>
+/* .w-screen {
+    width: 100vw;
+}
+
+.h-screen {
+    height: 100vh;
+} */
+/* 
+.h-full {
+    height: 100%;
+}
+
+.w-full {
+    width: 100%;
+} */
+
+body {
+    margin: 0;
+    padding: 0;
+}
+
+#map {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+}
+
+.marker {
+    background-image: url("http://localhost:3000/assets/images/download.png");
+    background-size: cover;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    cursor: pointer;
+}
+
+.mapboxgl-popup {
+    max-width: 200px;
+}
+
+.mapboxgl-popup-content {
+    text-align: center;
+    font-family: "Open Sans", sans-serif;
+}
+
+#menu {
+    position: absolute;
+    background: #EFEFEF;
+    padding: 10px;
+    font-family: "Open Sans", sans-serif;
+    margin-top: 600px;
+}
+</style>
